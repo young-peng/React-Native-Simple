4003b725de108663680669269cd846bc74bd1283
yao.peng <pengyao_2@163.com>
2016-07-18 20:28:10 +0800

    news list

---
+++ List.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class List extends Component {
+	render() {
+		return (
+			<View style={styles.list_item}>
+				<Text style={styles.list_item_font}>{this.props.title}</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	flex:{
+		flex:1
+	},
+	list_item:{
+		height:40,
+		marginLeft:10,
+		marginRight:10,
+		borderBottomWidth:1,
+		borderBottomColor:'#ddd',
+		justifyContent:'center'
+	},
+	list_item_font:{
+		fontSize:16
+	}
+});
